20bd6e6862cc9b12f13a61fb65e51822db522d5c
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-06-09 13:04:09 +0600

    database connect done

--- Server/index.js
+++ Server/index.js
@@ -6,11 +6,13 @@ const PORT = process.env.Server_PORT || 5000;
 
 const cors = require("cors");
 const router = require("./router");
+const DBConnect = require("./confiq/dbconfiq");
 
 app.use(cors());
 app.use(express.json());
 
 app.use(router);
+DBConnect();
 
 app.listen(PORT, () => {
   console.log("server is working");
