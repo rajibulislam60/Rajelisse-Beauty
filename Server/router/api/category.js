bb16c941ab0fefcaddbb9fd15f76b4e6e7c85856
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-06-28 21:42:37 +0600

    multer is working

--- Server/router/api/category.js
+++ Server/router/api/category.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const multer = require("multer");
 const { createCategory } = require("../../controller/categoryController");
 const router = express.Router();
 
-router.post("/createCategory", createCategory);
+const upload = multer({ dest: "uploads/" });
+
+router.post("/createCategory", upload.single("profile"), createCategory);
 
 module.exports = router;
