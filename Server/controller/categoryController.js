c8b3ff1520cab3f26e99031fefc9329c8bea75d7
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-08-16 22:58:51 +0600

    deleted sever area

--- Server/controller/categoryController.js
+++
@@ -1,6 +0,0 @@
-const createCategory = (req, res) => {
-  let { name, image, description } = req.body;
-  res.send(req.body);
-};
-
-module.exports = { createCategory };
--- Server/helpers/validateEmail.js
+++
@@ -1,5 +0,0 @@
-const EmailValidateCheck = (email) => {
-  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-};
-
-module.exports = EmailValidateCheck;
