2512d538843068c1392fbf42f5d91f685d0e6271
rajibulislam60 <rajibulislam60707877@gmail.com>
2025-08-17 00:39:08 +0600

    bashboard start newly

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
-import Home from "./pages/Home";
-import RootLayOut from "./RootLayOut";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<RootLayOut />}>
-            <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>Home</div>;
 };
 
 export default App;
